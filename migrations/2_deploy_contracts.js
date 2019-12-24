760c0f9b4e7f090c9cd97d5e9b31aab07904b543
Fevill <nouglozeh_gerard@yahoo.fr>
2019-12-24 11:49:29 +0100

    4. Token Sale

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,6 +1,11 @@
 const DappToken = artifacts.require("DappToken");
+const DappTokenSale = artifacts.require("DappTokenSale");
 
-module.exports = function(deployer) {
-  deployer.deploy(DappToken, 30000000);
+module.exports = function (deployer) {
+  deployer.deploy(DappToken, 30000000).then(function () {
+    //Token price is 0.004 ETH
+    var _tokenPrice = 4000000000000000;
+    return deployer.deploy(DappTokenSale,  DappToken.address,_tokenPrice);
+  });
 };
 
